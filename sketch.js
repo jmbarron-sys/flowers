53ebaba683235ff51c58eeb28d053a6d8cc3f473
jmbarron-sys <jmbarron@iu.edu>
2026-04-01 18:37:23 -0400

    Homepage

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
+let img;
+let img2;
+let img3;
+let img4;
+let img5;
+let img6;
+
+function preload(){
+ img = loadImage ('img/worm.png');
+ img2 = loadImage ('img/welcome.png');
+ img3 = loadImage ('img/flower.png');
+ img4 = loadImage ('img/rain.png');
+ img5 = loadImage ('img/line1.png');
+ img6 - loadImage ('img/line2.png')
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  ellipse(100, 100, 200, 200); 
+  background ('#F6C94C');
+  image(img,0,0,348,346);
+  image(img2,240,389,1036,160);
+  image(img3,740,304,270,670);
+  image(img4,770,-20,200,317)
+  image(img5,0,500,470,470)
+  image(img6,500,0, 450,630)
+
+        //displays the x and y position of the mouse on the canvas
+  fill(255) //white text
+  textSize (12);
+  text(`mouseX: ${mouseX}, mouseY: ${mouseY}`, 20, 20);
 }
